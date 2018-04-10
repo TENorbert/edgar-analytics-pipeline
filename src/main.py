b3ab69e0088775bd4dbea5c0b1e66b9eb501d658
TENorbert <delsubdue@gmail.com>
2018-04-09 23:49:25 -0500

    added more helper fxns: create_datetime, compare_datetime, get_elapse_time, create_request_document, Weblog class

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ def main():
 	inactivity_file = passed_files["inactivity"]
 	session_file = passed_files["session"]
 
-	vis_act = VisitorActivity()
+	vis_act = Weblog()
 	vis_act.compute_elapsed_time()
 	vis_act.compute_inactivity()
 
